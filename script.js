772d4b55d19facba3521d99fff85b12338438f8f
Faizz <119841230+faizshaikh17@users.noreply.github.com>
2024-08-25 14:11:47 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+let addButton = document.querySelector("button");
+let input = document.querySelector("input");
+let listConatiner = document.querySelector("#listContainer")
+
+
+addButton.addEventListener("click",()=>{
+    if(input.value == ""){
+        alert("Enter the Task");
+    }
+    else{console.log("hello")
+    let listItem = document.createElement("li");
+    listItem.innerHTML = input.value;
+    listConatiner.appendChild(listItem);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    listItem.appendChild(span);
+    }
+    
+
+    listConatiner.addEventListener("click", (e)=>{
+        if(e.target.tagName == "LI"){
+            e.target.classList.toggle("checked")
+            saveData();
+        }else if(e.target.tagName == "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    })
+
+})
+
+let saveData = () =>{
+    localStorage.setItem("data",listConatiner.innerHTML)
+}
+
+let showData = () => {
+    listConatiner.innerHTML = localStorage.getItem("data");
+}
+
+showData();
